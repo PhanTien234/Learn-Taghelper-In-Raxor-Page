1c920321c621085a084f766c828c18d624226c55
PhanTien234 <minhtienp328@gmail.com>
2023-04-06 09:52:20 +0700

    use lable, input taghelper and @html.lable, @Html.Textbox

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace taghelperexample.Pages;
@@ -6,6 +7,9 @@ namespace taghelperexample.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+
+    [DisplayName("Ten nguoi dung")]
+    public string UserName {get;set;} = "XuanThuLab";
 
     public IndexModel(ILogger<IndexModel> logger)
     {
